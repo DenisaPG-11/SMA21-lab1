409a36cb0a84091ad3852646454fd42780a02672
Denisa Pap <papdeni11@gmail.com>
2021-10-16 23:29:21 +0300

    added alertDialog for close button from Popup Window

--- app/src/main/java/com/example/laborator1_sma/MainActivity.java
+++ app/src/main/java/com/example/laborator1_sma/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.laborator1_sma;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.ViewGroup;
+
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
@@ -16,6 +17,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     TextView popup_text;
     ConstraintLayout main_page;
     Button closePopupBtn;
+
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         bPopup = findViewById(R.id.bPopup);
         eName =  findViewById(R.id.eName) ;
         tName =  findViewById(R.id.tName) ;
+
+        builder = new AlertDialog.Builder(this);
 
         bClick.setOnClickListener(v -> {
             String message = "Hello, " + eName.getText().toString() + "!" ;
@@ -70,7 +76,33 @@ public class MainActivity extends AppCompatActivity {
                 closePopupBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        popupWindow.dismiss();
+
+                        builder.setMessage("Do you want to close it?")
+                                .setCancelable(false)
+                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                       // finish();
+                                        Toast.makeText(getApplicationContext(),"you choose yes action for alertbox",
+                                                Toast.LENGTH_SHORT).show();
+                                        popupWindow.dismiss();
+                                    }
+                                })
+                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        //  Action for 'NO' Button
+                                        dialog.cancel();
+                                        Toast.makeText(getApplicationContext(),"you choose no action for alertbox",
+                                                Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+                        //Creating dialog box
+                        AlertDialog alert = builder.create();
+                        //Setting the title manually
+                        alert.setTitle("You will close this popup window :?");
+                        alert.show();
+
+                     //   popupWindow.dismiss();
                     }
                 });
 
